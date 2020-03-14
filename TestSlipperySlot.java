00cdeda15d9c6f39018f970bf4b6adbe8fdac632
victorialei21 <58520925+victorialei21@users.noreply.github.com>
2020-03-14 13:26:00 -0500

    Add files via upload

--- TestSlipperySlot.java
+++ TestSlipperySlot.java
@@ -2,7 +2,6 @@ import static org.junit.Assert.*;
 
 public class TestSlipperySlot {
 
-	@Test
 	public void sameFiveNumbers() {
 		SlipperySlot slot = new SlipperySlot();
 		int[] slotValues = new int[] {5, 5, 5, 5, 5};
@@ -10,7 +9,6 @@ public class TestSlipperySlot {
 		assertEquals(1000000-2, reward);
 	}
 	
-	@Test
 	public void sameFourNumbers() {
 		SlipperySlot slot = new SlipperySlot();
 		int[] slotValues = new int[] {5, 5, 5, 5, 4};
@@ -18,23 +16,4 @@ public class TestSlipperySlot {
 		assertEquals(10000-2, reward);
 	}
 	
-	@Test
-	public void fullHouse() {
-		
-	}
-	
-	@Test
-	public void threeSameTwoDiff() {
-		
-	}
-	
-	@Test
-	public void sameTwoNumbers() {
-		
-	}
-
-	@Test 
-	public void powerofTwo() {
-		
-	}
 }
